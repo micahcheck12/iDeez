b605c8db488d07926c0a8e021a5c6aeb2c75b25f
Micah Lawrence <micahdeterville@gmail.com>
2023-11-16 15:45:12 -0800

    Removed OAuth comments and removed Country to fix null issue

--- googleconnect.py
+++ googleconnect.py
@@ -8,17 +8,13 @@ import google_auth_oauthlib.flow
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 def channelid(my_channel_id):
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
+
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
     api_version = "v3"
-    #client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
 
-    # Get credentials and create an API client
-    #flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
-    #credentials = flow.run_console()
+
     youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey="AIzaSyAZn5LdXzYkpF2cp_qMaF6ei_TTDHxafFY")
 
 
@@ -35,7 +31,6 @@ def channelid(my_channel_id):
     "title": myitem["snippet"]["title"],
     "customUrl": myitem["snippet"]["customUrl"],
     "publishedAt": myitem["snippet"]["publishedAt"],
-    "country": myitem["snippet"]["country"],
     "viewCount": myitem["statistics"]["viewCount"],
     "subscriberCount": myitem["statistics"]["subscriberCount"],
     "videoCount": myitem["statistics"]["videoCount"]
@@ -49,18 +44,12 @@ if __name__ == "__main__":
 
 
 def videoid(video_id):
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
+
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
     api_version = "v3"
-    #client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
 
-    #Get credentials and create an API client
-    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        #client_secrets_file, scopes)
-    # credentials = flow.run_console()
     youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey="AIzaSyAZn5LdXzYkpF2cp_qMaF6ei_TTDHxafFY")
 
 

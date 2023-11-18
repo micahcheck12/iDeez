f553aef2361aaea75bb2812a2e4811caed887ba8
Micah Lawrence <micahdeterville@gmail.com>
2023-11-18 14:09:07 -0800

    Added reCaptcha logic to app.py and flash message to contact.html

--- app.py
+++ app.py
@@ -76,38 +76,51 @@ def contact():
 @app.route("/mail", methods=["GET", "POST"])
 def mail():
     if request.method == "POST":
-
         first_name = request.form.get('first_name')
         last_name = request.form.get('last_name')
         email = request.form.get('email')
-        message = request.form.get('message')  
-        
-        html_content = render_template("contactsubmission.html", 
-                                       first_name=first_name, 
-                                       last_name=last_name, 
-                                       email=email, 
-                                       message=message)
-        
-        msg = EmailMessage()
-        msg.set_content(html_content, subtype='html')  
+        message = request.form.get('message')
+        recaptcha_response = request.form.get('g-recaptcha-response')  
 
-        msg['Subject'] = f'iDeez Contact Submission'
-        msg['From'] = 'contactideez@gmail.com'
-        msg['To'] = 'contactideez@gmail.com'
+        recaptcha_secret_key = '6LdTGRQpAAAAALbj-oGiHAoPCI6s2oYjie0YmLB8'  
+        recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify'
+        recaptcha_data = {
+            'secret': recaptcha_secret_key,
+            'response': recaptcha_response,
+        }
 
-        s = smtplib.SMTP('smtp.gmail.com', 587)
+        recaptcha_result = requests.post(recaptcha_url, data=recaptcha_data)
+        recaptcha_json = recaptcha_result.json()
 
-        s.ehlo()
-        s.starttls()
+        if recaptcha_json['success']:
+            html_content = render_template("contactsubmission.html", 
+                                           first_name=first_name, 
+                                           last_name=last_name, 
+                                           email=email, 
+                                           message=message)
 
-        username = 'contactideez@gmail.com'
-        password = 'umvc rvif sodl lgrk'
-        s.login(username, password)
+            msg = EmailMessage()
+            msg.set_content(html_content, subtype='html')  
 
-        s.send_message(msg)
-        s.quit()
+            msg['Subject'] = f'iDeez Contact Submission'
+            msg['From'] = 'contactideez@gmail.com'
+            msg['To'] = 'contactideez@gmail.com'
 
-        return render_template("mailsent.html")
+            s = smtplib.SMTP('smtp.gmail.com', 587)
+            s.ehlo()
+            s.starttls()
+
+            username = 'contactideez@gmail.com'
+            password = 'umvc rvif sodl lgrk'
+            s.login(username, password)
+
+            s.send_message(msg)
+            s.quit()
+
+            return render_template("mailsent.html")
+        else:
+            flash('reCAPTCHA verification failed. Please try again.')
+            return render_template("contact.html")
 
     else:
         return render_template("contact.html")
